8f714211a88e951d0a4c6997679fac582b78b3fb
Krushel <72271923+Krushel@users.noreply.github.com>
2021-06-21 21:51:18 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .tasks import add
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.views.decorators.cache import cache_page
 
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+
+
+@cache_page(CACHE_TTL)
 def health_view(request):
     response = add('heal','thy')
     return HttpResponse(response)
