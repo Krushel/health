6adb38715ebd58bce91c9b14e9a661d0778c574a
Krushel <72271923+Krushel@users.noreply.github.com>
2021-06-21 22:07:09 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -137,7 +137,7 @@ CACHES = {
     },
 }
 
-SESSION_EXPIRATION = 600
+SESSION_EXPIRATION = 60 * 10
 
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
