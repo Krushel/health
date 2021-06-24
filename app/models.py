b86421151d89102cb8a33d4e70252a88847f4c2f
Krushel <72271923+Krushel@users.noreply.github.com>
2021-06-24 17:02:34 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Health(models.Model):
-    first_part = models.CharField(max_length=30)
-    last_part = models.CharField(max_length=30)
